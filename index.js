f88b7048447236e481aaef8920201f94656d65ed
Swapnil <swapnil118@icloud.com>
2024-12-23 14:54:57 -0500

    Practice with callback functions

--- index.js
+++ index.js
@@ -1,65 +1,93 @@
-// CLOSURE Example
-// Case 1:
-function outer() {
-  var a = 10;
+// // ---- CLOSURE EXAMPLE ----
+// // Case 1:
+// function outer() {
+//   var a = 10;
 
-  function inner() {
-    console.log(a);
-  }
-  var a = 20;
-  return inner;
+//   function inner() {
+//     console.log(a);
+//   }
+//   var a = 20;
+//   return inner;
+// }
+// // Global
+// var a = 30;
+// // Function call
+// var result = outer();
+// result();
+
+// // Case 2:
+// function outer1() {
+//   // var a = 10;
+
+//   function inner1() {
+//     console.log(a);
+//   }
+//   // var a = 20;
+//   return inner1;
+// }
+// // Global
+// var a = 30;
+
+// // Function call
+// var result1 = outer1();
+// result1();
+
+// // Case 3: Closure with Hoisting(output: undefined because of hoisting in local scope)
+// function outer2() {
+//   // var a = 20;
+//   function inner2() {
+//     console.log(a);
+//     var a = 10;
+//   }
+//   return inner2;
+// }
+// // Global
+// var a = 30;
+
+// // Function call
+// var result2 = outer2();
+// result2();
+
+// // Case 4:
+// function outer3() {
+//   // var a = 20;
+//   function inner3() {
+//     var a = 10;
+//     console.log(a);
+//   }
+//   return inner3;
+// }
+// // Global
+// var a = 30;
+
+// // Function call
+// var result3 = outer3();
+// result3();
+
+// ---- CALLBACK FUNCTION ----
+
+setTimeout(function () {
+  console.log("This is a Callback function with a timer");
+}, 4000);
+
+function a(x) {
+  console.log("I am function 'a'");
+  x();
 }
-// Global
-var a = 30;
-// Function call
-var result = outer();
-result();
 
-// Case 2:
-function outer1() {
-  // var a = 10;
+a(function x() {
+  console.log(
+    "I am going to be an argument to function 'a'. Refer me as callback function"
+  );
+});
 
-  function inner1() {
-    console.log(a);
-  }
-  // var a = 20;
-  return inner1;
+// ---- CLOSURE DEMO WITH EVENT LISTENERS ----
+
+function attachEventListener() {
+  let count = 0;
+  document.getElementById("clickMe").addEventListener("click", function s() {
+    console.log("Button clicked", count++);
+  });
 }
-// Global
-var a = 30;
 
-// Function call
-var result1 = outer1();
-result1();
-
-// Case 3: Closure with Hoisting(output: undefined because of hoisting in local scope)
-function outer2() {
-  // var a = 20;
-  function inner2() {
-    console.log(a);
-    var a = 10;
-  }
-  return inner2;
-}
-// Global
-var a = 30;
-
-// Function call
-var result2 = outer2();
-result2();
-
-// Case 4:
-function outer3() {
-  // var a = 20;
-  function inner3() {
-    var a = 10;
-    console.log(a);
-  }
-  return inner3;
-}
-// Global
-var a = 30;
-
-// Function call
-var result3 = outer3();
-result3();
+attachEventListener();
