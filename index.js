1a6ca712291f6345c0b1036f96fc15a3967e2a45
Swapnil <swapnil118@icloud.com>
2025-01-10 22:21:42 -0500

    Currying function with closures

--- index.js
+++ index.js
@@ -211,13 +211,26 @@
 
 // ---------------------------- CURRYING FUNCTION -----------------------
 
-let multiply = function (x, y) {
-  console.log(x * y);
+// let multiply = function (x, y) {
+//   console.log(x * y);
+// };
+
+// // WITH BIND METHOD
+// let multiplyByTwo = multiply.bind(this, 2);
+// multiplyByTwo(3);
+
+// let multiplyByThree = multiply.bind(this, 3);
+// multiplyByThree(3);
+
+// WITH CLOSURES
+let multiply = function (x) {
+  return function (y) {
+    console.log(x * y);
+  };
 };
-// WITH BIND METHOD
 
-multiplyByTwo = multiply.bind(this, 2);
-multiplyByTwo(3);
+let multiplyByTwo = multiply(2);
+multiplyByTwo(2);
 
-multiplyByThree = multiply.bind(this, 3);
-multiplyByThree(3);
+let multiplyByThree = multiply(3);
+multiplyByThree(2);
