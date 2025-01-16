f402f424f0a6026a1acec2bf4ab427e10ca968f4
Swapnil <swapnil118@icloud.com>
2025-01-16 16:09:35 -0500

    Making a generic function

--- index.js
+++ index.js
@@ -338,3 +338,30 @@ const calculateCircumference = function (radius) {
 };
 
 console.log(calculateCircumference(radius));
+
+// Diameter of circle
+const calculateDiameter = function (radius) {
+  const output = [];
+  for (let i = 0; i < radius.length; i++) {
+    output.push(2 * radius[i]);
+  }
+  return output;
+};
+console.log(calculateDiameter(radius));
+
+// MAKING A GENERIC FUNCTION OF ABOVE FUNCTION
+
+const calculate = function (radius, logic) {
+  const output = [];
+  for (let i = 0; i < radius.length; i++) {
+    output.push(logic(radius[i]));
+  }
+  return output;
+};
+
+// Function to calculate area
+const area = function (radius) {
+  return Math.PI * radius * radius;
+};
+// Calling
+console.log(calculate(radius, area));
