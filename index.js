86174a822924f9fb6bc68652172282906999456a
Swapnil <swapnil118@icloud.com>
2025-02-17 22:30:16 -0500

    CSS Box Model

--- index.js
+++ index.js
@@ -61,6 +61,7 @@
 //  - Loop: Programming way of running a code until a certain condition is met
 //  - for of and for in loop
 //  - 7 types of functions in JS: Named fn, Anonymous fn, Function expression, Arrow functions, IIFE, Callback fn, Higher-Order function
+//  - CSS Box Model
 
 //           ---------------------- END  --------------------------
 
